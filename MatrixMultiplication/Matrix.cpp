2e1ccce2bf58618ddc4b43f28a46e16a79359684
Nikolay Sarychev <abeliandevelopment@gmail.com>
2013-02-10 12:07:25 -0700

    C - adding matrix multiplication

---
+++ MatrixMultiplication/Matrix.cpp
@@ -0,0 +1,17 @@
+/*
+ * Matrix.cpp
+ *
+ *  Created on: Feb 12, 2010
+ *      Author: nikolay
+ */
+
+#include "Matrix.h"
+
+Matrix::Matrix() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Matrix::~Matrix() {
+	// TODO Auto-generated destructor stub
+}
---
+++ MatrixMultiplication/Matrix.h
@@ -0,0 +1,16 @@
+/*
+ * Matrix.h
+ *
+ *  Created on: Feb 12, 2010
+ *      Author: nikolay
+ */
+
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+class Matrix {public:
+	Matrix();
+	virtual ~Matrix();
+};
+
+#endif /* MATRIX_H_ */
